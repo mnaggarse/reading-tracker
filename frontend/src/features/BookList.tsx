cf703907d65a56cf83daa17c809deb460b94caa5
mnaggarse <mnaggarse@gmail.com>
2025-07-16 22:24:55 +0300

    Make books come from database

--- frontend/src/features/BookList.tsx
+++ frontend/src/features/BookList.tsx
@@ -1,14 +1,30 @@
 import { BookCard } from "@/components/BookCard";
+import supabase from "@/utils/supabase";
+import { useEffect, useState } from "react";
 
 export default function BookList() {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const { data, error } = await supabase.from("books").select("*");
+
+    if (error) {
+      console.error("Error fetching books:", error);
+    } else {
+      setBooks(data);
+      console.log(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <div className="p-6 mx-auto max-w-5xl grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
+      {books.map((book) => (
+        <BookCard key={book.id} book={book} />
+      ))}
     </div>
   );
 }
