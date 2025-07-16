de492381449b451f73a22971e0bffb1c32159d6b
mnaggarse <mnaggarse@gmail.com>
2025-07-16 22:24:36 +0300

    Get data from database and fix image

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -2,16 +2,27 @@ import { Card, CardBody, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { Progress } from "@heroui/progress";
 
-export function BookCard() {
+export function BookCard({ book }) {
+  const progress = (book.read / book.pages) * 100;
+
   return (
-    <Card fullWidth isPressable>
-      <CardHeader>
-        <Image src="https://tmm.chicagodistributioncenter.com/IsbnImages/9780226822952.jpg" />
+    <Card
+      fullWidth
+      isPressable
+      className="cursor-pointer"
+      isDisabled={progress === 100}
+    >
+      <CardHeader className="w-full">
+        <Image className="object-cover w-full" src={book.cover} />
       </CardHeader>
       <CardBody className="pt-0">
-        <h2 className="text-lg font-bold">Atomic Habits</h2>
-        <p>James Clear</p>
-        <Progress className="mt-2" value={30} />
+        <h2 className="text-lg font-bold">{book.title}</h2>
+        <p>{book.author}</p>
+        <Progress
+          className="mt-2"
+          color={progress === 100 ? "success" : "primary"}
+          value={progress}
+        />
       </CardBody>
     </Card>
   );
