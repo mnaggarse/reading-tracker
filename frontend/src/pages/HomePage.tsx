dcd9123ad858ce4cdbf8b3cf9126e9700b2ec594
mnaggarse <mnaggarse@gmail.com>
2025-07-16 15:59:12 +0300

    Add HomePage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Nav from "@/components/Nav";
+import BookList from "@/features/BookList";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Nav />
+      <BookList />
+    </div>
+  );
+}
