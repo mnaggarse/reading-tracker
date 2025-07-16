dad0468e82db5059c7bfce60d80afbefa9cd1fe5
mnaggarse <mnaggarse@gmail.com>
2025-07-16 22:24:07 +0300

    Add AddButton

---
+++ frontend/src/components/AddButton.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@heroui/button";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@heroui/dropdown";
+import { Edit, Plus, Search } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+export default function AddButton() {
+  const navigate = useNavigate();
+
+  const navigateToAddPage = () => {
+    navigate("/add");
+  };
+
+  const navigateToSearchPage = () => {
+    navigate("/search");
+  };
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button
+          isIconOnly
+          className="fixed bottom-10 right-8 z-50"
+          color="primary"
+          radius="full"
+          size="lg"
+        >
+          <Plus size={30} />
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Dropdown menu with icons" variant="faded">
+        <DropdownItem
+          key="manual"
+          startContent={<Edit />}
+          onPress={navigateToAddPage}
+        >
+          Manual add
+        </DropdownItem>
+        <DropdownItem
+          key="search"
+          startContent={<Search />}
+          onPress={navigateToSearchPage}
+        >
+          Search for a book
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
