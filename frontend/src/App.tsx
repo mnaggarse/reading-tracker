fa84b8808f15ebe4cbb4a963feba2d24314c6164
mnaggarse <mnaggarse@gmail.com>
2025-07-16 22:36:48 +0300

    Add HomePage, SignupPage and LoginPage routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SearchPage from "./pages/SearchPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<HomePage />} path="/" />
+      <Route element={<SearchPage />} path="/search" />
+      <Route element={<SignupPage />} path="/signup" />
+      <Route element={<LoginPage />} path="/login" />
     </Routes>
   );
 }
