34baf5d4acca85cdd4ede972227a680d08eaa3d9
mnaggarse <mnaggarse@gmail.com>
2025-07-17 18:38:08 +0300

    Show welocme message and signout button

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,16 +1,35 @@
-import { Avatar, Navbar, NavbarBrand, NavbarContent } from "@heroui/react";
-import { Settings } from "lucide-react";
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@heroui/react";
+import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext.jsx";
 
-export default function Nav() {
+const Nav = () => {
+  const navigate = useNavigate();
+  const { session, signout } = UserAuth();
+  const name = session.user.user_metadata.username;
+
+  const handleSignOut = async () => {
+    try {
+      await signout();
+      navigate("/login");
+    } catch (err) {
+      console.error("Sign out error:", err);
+      alert("Failed to sign out. Please try again.");
+    }
+  };
+
   return (
     <Navbar className="bg-white border-b border-gray-200">
       <NavbarBrand>
-        <Avatar className="cursor-pointer" name="MA" />
+        <h1 className="text-lg font-bold">Welcome, {name}</h1>
       </NavbarBrand>
 
       <NavbarContent justify="end">
-        <Settings className="cursor-pointer" />
+        <Button variant="faded" color="danger" onPress={handleSignOut} className="font-semibold">
+          Sign out
+        </Button>
       </NavbarContent>
     </Navbar>
   );
-}
+};
+
+export default Nav;
