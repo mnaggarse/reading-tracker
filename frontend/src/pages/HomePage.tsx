04cce22d3a239c06e6503f1d7da5d5c8a4d2f417
mnaggarse <mnaggarse@gmail.com>
2025-07-16 22:50:50 -0500

    Add edit book

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@heroui/button";
+import { Card, CardBody, CardHeader } from "@heroui/card";
+import { Image } from "@heroui/image";
+import { Input } from "@heroui/input";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/modal";
+import { Progress } from "@heroui/progress";
 import { User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import AddButton from "@/components/AddButton";
-import { BookCard } from "@/components/BookCard";
 import Nav from "@/components/Nav";
 import supabase from "@/utils/supabase";
 
@@ -12,8 +23,17 @@ export default function HomePage() {
   const [user, setUser] = useState<User | null>(null);
   const [books, setBooks] = useState<any[]>([]);
   const [loadingUser, setLoadingUser] = useState(true);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [selectedBook, setSelectedBook] = useState<any | null>(null);
+  const [formValues, setFormValues] = useState({
+    title: "",
+    author: "",
+    pages: "",
+    read: "",
+    cover: "",
+  });
+
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
     const getUser = async () => {
@@ -76,19 +96,167 @@ export default function HomePage() {
     await supabase.auth.signOut();
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <>
       <Nav />
+
       <div className="p-6 mx-auto max-w-5xl columns-2 md:columns-3 lg:columns-4 gap-4 md:gap-8">
-        <Button className="fixed top-3 left-45 z-50" onPress={handleLogout}>
-          Logout
-        </Button>
-        {books.map((book) => (
-          <div key={book.id} className="mb-4">
-            <BookCard book={book} />
-          </div>
+        {books.map((book, index) => (
+          <Card
+            key={index}
+            fullWidth
+            isPressable
+            className="cursor-pointer mb-4"
+            isDisabled={(book.read / book.pages) * 100 === 100}
+            onPress={() => {
+              setSelectedBook(book);
+              setFormValues({
+                title: book.title || "",
+                author: book.author || "",
+                cover: book.cover || "",
+                pages: book.pages || 0,
+                read: book.read || 0,
+              });
+              onOpen();
+            }}
+          >
+            <CardHeader>
+              <Image src={book.cover} width={500} />
+            </CardHeader>
+            <CardBody className="pt-0">
+              <h2 className="text-lg font-bold">{book.title}</h2>
+              <p>{book.author}</p>
+              <Progress
+                className="mt-2"
+                color={
+                  (book.read / book.pages) * 100 === 100 ? "success" : "primary"
+                }
+                value={(book.read / book.pages) * 100}
+              />
+            </CardBody>
+          </Card>
         ))}
       </div>
+
+      <Modal
+        className="w-[90%]"
+        isOpen={isOpen}
+        placement="center"
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Edit Book</ModalHeader>
+              <ModalBody className="flex gap-4">
+                <Input
+                  label="Title"
+                  labelPlacement="outside-top"
+                  name="title"
+                  size="lg"
+                  value={formValues.title}
+                  variant="bordered"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  label="Author"
+                  labelPlacement="outside-top"
+                  name="author"
+                  size="lg"
+                  value={formValues.author}
+                  variant="bordered"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  label="Pages"
+                  labelPlacement="outside-top"
+                  name="pages"
+                  size="lg"
+                  type="number"
+                  value={formValues.pages}
+                  variant="bordered"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  label="Read"
+                  labelPlacement="outside-top"
+                  name="read"
+                  size="lg"
+                  type="number"
+                  value={formValues.read}
+                  variant="bordered"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  label="Cover"
+                  labelPlacement="outside-top"
+                  name="cover"
+                  size="lg"
+                  type="text"
+                  value={formValues.cover}
+                  variant="bordered"
+                  onChange={handleInputChange}
+                />
+              </ModalBody>
+              <ModalFooter className="flex justify-between">
+                <Button
+                  color="danger"
+                  size="lg"
+                  variant="flat"
+                  onPress={onClose}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  color="primary"
+                  size="lg"
+                  onPress={async () => {
+                    if (!selectedBook) return;
+
+                    const updatedBook = {
+                      title: formValues.title,
+                      author: formValues.author,
+                      cover: formValues.cover,
+                      pages: parseInt(formValues.pages),
+                      read: parseInt(formValues.read),
+                    };
+
+                    const { error } = await supabase
+                      .from("books")
+                      .update(updatedBook)
+                      .eq("id", selectedBook.id)
+                      .eq("user_id", user.id); // ensure user owns the book
+
+                    if (error) {
+                      console.error("Failed to update book:", error.message);
+
+                      return;
+                    }
+
+                    // Update book in UI
+                    setBooks((prevBooks) =>
+                      prevBooks.map((b) =>
+                        b.id === selectedBook.id ? { ...b, ...updatedBook } : b
+                      )
+                    );
+
+                    onClose();
+                  }}
+                >
+                  Save
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+
       <AddButton />
     </>
   );
